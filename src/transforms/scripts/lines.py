96506ce2b98f3d9e9597a92ac6f328e405c7aae9
theunistudent <z5257559@ad.unsw.edu.au>
2023-10-27 17:12:39 +1100

    lines is mostly writen, could be cleaned up. Have not had a chance to test functionality. Most deffinatly will need some debuging.

--- src/transforms/scripts/lines.py
+++ src/transforms/scripts/lines.py
@@ -17,14 +17,18 @@ from ros2_aruco_interfaces.msg import ArucoMarkers
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 from custom_messages import RobotAction
 
+import cv2
+import numpy as np
 
-class lines(Node):
+
+class lines(rclpy.Node):
 
     def __init__(self):
-    super().__init__('line')
-    self.subscription = self.create_subscription(RobotAction, 'robot_action', self.topic_callback, 10 )
-    tf_broadcaster_ = tf2_ros.TransformBroadcaster(node)
-    tf_listener = TransformListener(node)
+        super().__init__('line')
+        self.subscription = self.create_subscription(RobotAction, 'robot_action', self.topic_callback, 10)
+        self.tf_broadcaster_ = TransformBroadcaster(self)  # Pass 'self' to the constructor
+        self.tf_listener = TransformListener(self)  # Pass 'self' to the constructor
+
 
 
     def topic_callback(self, msg):
@@ -43,18 +47,80 @@ class lines(Node):
         target_frame = "base_frame"
         corner3 = tf_listener.lookup_transform(target_frame, source_frame, rclpy.time.Time())
 
+        source_frame = "paper_corner_4"
+        target_frame = "base_frame"
+        corner4 = tf_listener.lookup_transform(target_frame, source_frame, rclpy.time.Time())
+
 
         # need to check if these produce messurments in the right sign. Could have gotten confuesed with axies orientations.
         paper_hight = corner1.transform.translation.x - corner2.transform.translation.x
         paper_lenght = corner1.transform.translation.x - corner3.transform.translation.x
         paper_ratio = paper_hight / paper_lenght
 
+        # will add "crop" from camera length resolution "x" variable.
+        # this will stop lines from warping during transfromation by keeping the hight/lenght ratio the same as the paper
+        cam_hight = 720
+        cam_lenght = 1280
+        crop = paper_ratio*cam_lenght/cam_hight
+        cam_lenght = cam_lenght + crop
+
+        # might need to recenter the robot_action positions
+
+        # get transform 
+            # Define the source points (original points)
+        src_points = np.array([
+            [corner1.transform.translation.x, corner1.transform.translation.y],  # Point 1
+            [corner2.transform.translation.x, corner2.transform.translation.y],  # Point 2
+            [corner3.transform.translation.x, corner3.transform.translation.y],  # Point 3
+            [corner4.transform.translation.x, corner4.transform.translation.y]   # Point 4
+        ], dtype=np.float32)
+        # Define the destination points (desired points)
+        dst_points = np.array([
+            [cam_lenght, cam_hight],  # New position for Point 1
+            [cam_lenght, 0],  # New position for Point 2
+            [0, 0],  # New position for Point 3
+            [0, cam_hight]   # New position for Point 4
+        ], dtype=np.float32)
+        # Find the perspective transformation matrix (homography)
+        H, _ = cv2.findHomography(src_points, dst_points)
+
+        # The matrix H now contains the transformation from src_points to dst_points
+        print("Transformation Matrix (Homography):")
+        print(H)
+
+        # take homography matrix and multiply it by the robot_action points to get global points
+        index = iter(msg.x)
+        for i in index:
+            transformed_point = np.dot(H, [msg.x[i], msg.y[i], 1])
+
+            # Access the transformed coordinates
+            transformed_x, transformed_y, w = transformed_point
+            # Normalize the coordinates (divide by w)
+            transformed_x /= w
+            transformed_y /= w
+
+            # Create a dynamic transformation from "base_frame" to "child_frame"
+            dynamic_transform = TransformStamped()
+            dynamic_transform.header.frame_id = 'base_frame'
+            dynamic_transform.child_frame_id = 'robot_command'
+            dynamic_transform.transform.translation.x = transformed_x  # Update translation
+            dynamic_transform.transform.translation.y = transformed_y
+            dynamic_transform.transform.translation.z = 10.0
+            dynamic_transform.transform.rotation.w = 0.0  # Update rotation
+
+            # Set the timestamp
+            dynamic_transform.header.stamp = node.get_clock().now().to_msg()
+
+            # Broadcast the dynamic transformation
+            tf_broadcaster.sendTransform(dynamic_transform)
 
 
 
-        # index = iter(msg.x)
-        # for i in index
 
+
+
+
+        
 
 
 
@@ -62,7 +128,7 @@ class lines(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = BallDetectionNode()
+    node = lines()
     rclpy.spin(node)
     rclpy.shutdown()
 

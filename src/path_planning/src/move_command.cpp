0c9815b7c7e52a6434c8229e57c6174166375671
Daniel-Horsnell <z5259702@ad.unsw.edu.au>
2023-11-15 22:42:15 +1100

    Added end-effector and pen rack

--- src/path_planning/src/move_command.cpp
+++ src/path_planning/src/move_command.cpp
@@ -14,8 +14,38 @@
 #include "custom_messages/msg/robot_action.hpp"
 #include <rviz_visual_tools/rviz_visual_tools.hpp>
 #include <moveit_visual_tools/moveit_visual_tools.h>
+#include <cmath>
 
 using namespace std::chrono_literals;
+
+
+struct Quaternion {
+    double w, x, y, z;
+};
+
+Quaternion convertRPYtoQuaternion(double roll, double pitch, double yaw) {
+    // Convert angles from degrees to radians
+    roll = roll * M_PI / 180.0;
+    pitch = pitch * M_PI / 180.0;
+    yaw = yaw * M_PI / 180.0;
+
+    // Calculate half angles
+    double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+
+    // Calculate quaternion components
+    Quaternion q;
+    q.w = cr * cp * cy + sr * sp * sy;
+    q.x = sr * cp * cy - cr * sp * sy;
+    q.y = cr * sp * cy + sr * cp * sy;
+    q.z = cr * cp * sy - sr * sp * cy;
+
+    return q;
+}
 
 //Function to generate a collision object
 auto generateCollisionObject(float sx,float sy, float sz, float x, float y, float z, std::string frame_id, std::string id) {
@@ -32,6 +62,34 @@ auto generateCollisionObject(float sx,float sy, float sz, float x, float y, floa
 
   geometry_msgs::msg::Pose box_pose;
   box_pose.orientation.w = 1.0; 
+  box_pose.position.x = x;
+  box_pose.position.y = y;
+  box_pose.position.z = z;
+
+  collision_object.primitives.push_back(primitive);
+  collision_object.primitive_poses.push_back(box_pose);
+  collision_object.operation = collision_object.ADD;
+  return collision_object;
+}
+
+//Function to generate a collision object
+auto generateCollisionObjectCylinder(float r,float h, float x, float y, float z, float roll, float pitch, float yaw, std::string frame_id, std::string id) {
+  moveit_msgs::msg::CollisionObject collision_object;
+  collision_object.header.frame_id = frame_id;
+  collision_object.id = id;
+  shape_msgs::msg::SolidPrimitive primitive;
+
+  primitive.type = primitive.CYLINDER;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[primitive.CYLINDER_RADIUS] = r;
+  primitive.dimensions[primitive.CYLINDER_HEIGHT] = h;
+
+  geometry_msgs::msg::Pose box_pose;
+  auto quat = convertRPYtoQuaternion(roll, pitch, yaw);
+  box_pose.orientation.x =quat.x;
+  box_pose.orientation.y =quat.y;
+  box_pose.orientation.z =quat.z;
+  box_pose.orientation.w =quat.w;
   box_pose.position.x = x;
   box_pose.position.y = y;
   box_pose.position.z = z;
@@ -95,12 +153,28 @@ class move_command : public rclcpp::Node
       auto col_object_backWall = generateCollisionObject( 2.4, 0.04, 1.0, 0.85, -0.30, 0.5, frame_id, "backWall");
       auto col_object_sideWall = generateCollisionObject( 0.04, 1.2, 1.0, -0.30, 0.25, 0.5, frame_id, "sideWall");
       auto col_object_table = generateCollisionObject( 2.4, 1.2, 0.04, 0.85, 0.25, -0.03, frame_id, "table");
+      auto col_object_rack = generateCollisionObject(0.1, 0.4, 0.07, 0.0, 0.15, 0.0, "pen_rack_4", "marker_rack");
+      auto col_object_end_effector_box = generateCollisionObject( 0.022, 0.08, 0.075, 0.0, 0.0, 0.0375, "wrist_3_link", "end_effector_box");
+      auto col_object_end_effector_iris = generateCollisionObjectCylinder(0.035, 0.017, 0.0, -0.04, 0.09, 0.0, 0.0, 0.0, "wrist_3_link", "end_effector_iris");
+      auto col_object_end_effector_shaft = generateCollisionObjectCylinder(0.0175, 0.09, 0.0, -0.04, 0.045, 0.0, 0.0, 0.0, "wrist_3_link", "end_effector_shaft");
 
       moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
       // Apply table as a collision object
       planning_scene_interface.applyCollisionObject(col_object_backWall);
       planning_scene_interface.applyCollisionObject(col_object_sideWall);
       planning_scene_interface.applyCollisionObject(col_object_table);
+      planning_scene_interface.applyCollisionObject(col_object_rack);
+      planning_scene_interface.applyCollisionObject(col_object_end_effector_box);
+      planning_scene_interface.applyCollisionObject(col_object_end_effector_iris);
+      planning_scene_interface.applyCollisionObject(col_object_end_effector_shaft);
+
+      auto color = std::make_shared<std_msgs::msg::ColorRGBA>();
+      color->r = 1.0;  // Red
+      color->g = 0.0;  // Green
+      color->b = 0.0;  // Blue
+      color->a = 0.0;  // Alpha (transparency)
+
+      
       // Publish to /arduinoCommand
       commands_publisher_ = create_publisher<std_msgs::msg::String>("/arduinoCommand", 10);
       timer_ = this->create_wall_timer(
@@ -164,11 +238,7 @@ class move_command : public rclcpp::Node
     void move_pen(bool close_gripper, tf2::Quaternion q, geometry_msgs::msg::Pose home) {
       std::vector<geometry_msgs::msg::Pose> waypoints;
       moveit_msgs::msg::RobotTrajectory trajectory;
-          // Pick up the pen
-      //get the transform
-      
 
-      
       waypoints.push_back(home);
       // get offset position on rack
       
@@ -259,6 +329,38 @@ class move_command : public rclcpp::Node
         pen_y = pen_y + (t.transform.translation.y - pen_y)/5;
         pen_yaw = pen_yaw + (yaw-pen_yaw)/5;
       }
+
+      // try {
+      //   t = tf_buffer_->lookupTransform(
+      //     "paper_corner_0", fromFrameRel,
+      //     tf2::TimePointZero);
+      // } catch (const tf2::TransformException & ex) {
+      //   RCLCPP_INFO(
+      //     this->get_logger(), "Could not transform %s to %s: %s",
+      //     toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
+      //   return;
+      // }
+      // Eigen::Vector3d point1;
+      // point1.x() = t.transform.translation.x;
+      // point1.y() = t.transform.translation.y;
+      // point1.z() = t.transform.translation.z;
+
+      // try {
+      //   t = tf_buffer_->lookupTransform(
+      //     "paper_corner_2", fromFrameRel,
+      //     tf2::TimePointZero);
+      // } catch (const tf2::TransformException & ex) {
+      //   RCLCPP_INFO(
+      //     this->get_logger(), "Could not transform %s to %s: %s",
+      //     toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
+      //   return;
+      // }
+      // Eigen::Vector3d point2;
+      // point2.x() = t.transform.translation.x;
+      // point2.y() = t.transform.translation.y;
+      // point2.z() = t.transform.translation.z;
+      // std::cout << point2.x() << point2.y() << point2.z() << std::endl;
+      // visual_tools_->publishCuboid(point1, point2, rviz_visual_tools::RED);
     }
       
 
